1f24ba181c40e1451091c17fbed8afcae688ac19
unknown <satya-1298>
2023-04-11 16:11:19 +0530

    TestMethod

---
+++ MAnalyserTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿
+using MAnalyser;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace MAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string message = "Iam in Sad mood";
+            Mood mood = new Mood(message);
+            string r = mood.MoodAnalyser();
+            string actualoutput = "SAD";
+            string expectedoutput;
+            expectedoutput = mood.MoodAnalyser();
+            Assert.AreEqual(actualoutput, expectedoutput);
+        }
+        [TestMethod]
+
+        public void TestMethod2()
+        {
+            string message1 = "Iam in Any mood";
+            Mood mood = new Mood(message1);
+            string r = mood.MoodAnalyser();
+            string actualoutput = "Happy";
+            string expectedoutput;
+            expectedoutput = mood.MoodAnalyser();
+            Assert.AreEqual(actualoutput, expectedoutput);
+        }
+
+    }
+
+}
