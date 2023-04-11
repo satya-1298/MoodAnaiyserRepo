c8cc6dc36e63218b2ad77e9c4f9d6f83785e8026
satya-1298 <124281275+satya-1298@users.noreply.github.com>
2023-04-11 15:48:25 +0530

    Update UnitTest1.cs

--- MAnalyserTest/UnitTest1.cs
+++ MAnalyserTest/UnitTest1.cs
@@ -8,11 +8,11 @@ namespace MAnalyserTest
         public void TestMethod1()
         {
             string message = "Iam in Sad mood";
-            Mood mood = new Mood(message);
-            string r = mood.MoodAnalyser();
+            Mood mood = new Mood();
+            string r = mood.MoodAnalyser(message);
             string actualoutput = "SAD";
             string expectedoutput;
-            expectedoutput = mood.MoodAnalyser();
+            expectedoutput = mood.MoodAnalyser(message);
             Assert.AreEqual(actualoutput, expectedoutput);
         }
         [TestMethod]
@@ -20,11 +20,11 @@ namespace MAnalyserTest
         public void TestMethod2()
         {
             string message1 = "Iam in Any mood";
-            Mood mood = new Mood(message1);
-            string r = mood.MoodAnalyser();
+            Mood mood = new Mood();
+            string r = mood.MoodAnalyser(messsage1);
             string actualoutput = "Happy";
             string expectedoutput;
-            expectedoutput = mood.MoodAnalyser();
+            expectedoutput = mood.MoodAnalyser(message1);
             Assert.AreEqual(actualoutput, expectedoutput);
         }
     
