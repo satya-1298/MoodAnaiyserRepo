72ccb013308a5ce97fa4b5ff087e537ab4c04e84
unknown <satya-1298>
2023-04-11 14:40:38 +0530

    Analyse and respond sad or happy

---
+++ MAnalyser/Mood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAnalyser
+{
+    public class Mood
+    {
+        private string message;
+        public  Mood(string message)
+        {
+            this.message = message;
+        }
+        public string MoodAnalyser()
+        {
+           
+            if(this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+    }
+}
