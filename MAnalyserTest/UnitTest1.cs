30fb6f3423926adf95e65ad270b608ffa792df4b
unknown <satya-1298>
2023-04-12 18:11:40 +0530

    TestMethod of UC4

--- MAnalyserTest/UnitTest1.cs
+++ MAnalyserTest/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace MAnalyserTest
             string message = "Iam in Sad mood";
             Mood mood = new Mood(message);
             string r = mood.MoodAnalyser();
-            string actualoutput = "SAD";
+            string actualoutput = "Happy";
             string expectedoutput;
             expectedoutput = mood.MoodAnalyser();
             Assert.AreEqual(actualoutput, expectedoutput);
@@ -25,7 +25,7 @@ namespace MAnalyserTest
 
         public void TestMethod2()
         {
-            string message1 = "Iam in Any mood";
+            string message1 = "Iam in Happy mood";
             Mood mood = new Mood(message1);
             string r = mood.MoodAnalyser();
             string actualoutput = "Happy";
@@ -46,6 +46,7 @@ namespace MAnalyserTest
             Assert.AreEqual(actualoutput, expectedoutput);
 
         }
+
         [TestMethod]
         public void TestMethod4() 
         {
@@ -64,6 +65,50 @@ namespace MAnalyserTest
             }
         }
 
+        //TC 4.1
+
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+
+            string message = null;
+            object expected = new Mood(message);
+            object obj = MoodAnalyzerFactory.CreateMoodAnalyse("MAnalyser.MoodAnalyse", "MoodAnalyse");
+           Assert.AreEqual(expected, obj);
+        }
+
+        //TC 4.2 : Given Improper class name should throw MoodAnaliserException.
+        [TestMethod]
+        public void GivenImproperClassNameShouldThrowMoodAnalyserException()
+        {
+            string Actual = "Class not found";
+            try
+            {
+                object moodAnalyserObject = MoodAnalyzerFactory.CreateMoodAnalyse("MoodAnalyzerApp.DemoClass", "DemoClass");
+            }
+            catch (MoodException ex)
+            {
+                Assert.AreEqual(Actual, ex.Message);
+            }
+        }
+
+        //TC 4.3 : Given improper constructor should not throw MoodAnalyserException.
+        [TestMethod]
+        public void GivenImproperConstructorShouldThrowMoodAnalyserException()
+        {
+            string excepted = "Constructor not found";
+            try
+            {
+                object moodAnalyserObject = MoodAnalyzerFactory.CreateMoodAnalyse("DemoClass", "MoodAnalyse");
+            }
+            catch (MoodException exception)
+            {
+                Assert.AreEqual(excepted, exception.Message);
+            }
+        }
+
     }
 
 }
+
+
