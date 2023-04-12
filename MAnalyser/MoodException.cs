f9a1853882fad5d55d047e3039ef2cf4cfedfe26
unknown <satya-1298>
2023-04-12 14:34:04 +0530

    create Custom Exception

---
+++ MAnalyser/MoodException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAnalyser
+{
+    public class MoodException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MOOD, EMPTY_MOOD
+        }
+        public ExceptionType exceptionType;
+        public MoodException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
